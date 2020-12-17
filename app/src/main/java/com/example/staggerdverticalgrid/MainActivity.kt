f819074779079bf2fdae05a6dcd114682a3e22c1
elye <elye.project@gmail.com>
2020-12-17 21:33:00 +1100

    Initial commit

---
+++ app/src/main/java/com/example/staggerdverticalgrid/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.staggerdverticalgrid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.ripple.rememberRippleIndication
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val columnWidth = mutableStateOf((50..220).random())
+
+            Column {
+                Button(onClick = {
+                    columnWidth.value = (50..220).random()
+                }, modifier = Modifier.padding(6.dp)) {
+                    Text(
+                        text = "Generate",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+                ScrollableColumn {
+                    StaggeredVerticalGrid(
+                        maxColumnWidth = columnWidth.value.dp,
+                        modifier = Modifier.padding(4.dp)
+                    ) {
+                        (1..100).forEach {
+                            Card(it.toString(), columnWidth)
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Card(
+    id: String,
+    state: MutableState<Int>,
+    modifier: Modifier = Modifier
+) {
+    val color = remember(state.value) {
+        mutableStateOf(Color((0xFF000000..0xFFFFFFFF).random()))
+    }
+
+    Surface(
+        modifier = modifier.padding(4.dp).clickable(
+            onClick = { },
+            indication = rememberRippleIndication(
+                color = color.value.rippleColor()
+            )
+        ),
+        color = color.value,
+        elevation = 8.dp,
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column (modifier = Modifier
+            .height((50..200).random().dp)
+            .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = id,
+                textAlign = TextAlign.Center,
+                color = color.value.rippleColor(),
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+private fun Color.whiteness(): Float {
+    return (red + green + blue) / 3
+}
+
+private fun Color.rippleColor(): Color {
+    return if (whiteness() >= 0.5) Color.Black else Color.White
+}
